b1fb8a1e7e4803b9f57c840fecf30290f708782e
KTDRAL <angel.catedral.1410@gmail.com>
2020-05-30 18:46:08 -0500

    Correcciones de login y registro. Agregado script para verificar sesión en todas las páginas

---
+++ negocios/js/shared/verificar_sesion/verificar_sesion.js
@@ -0,0 +1,10 @@
+if (localStorage.getItem("sesion")) {
+    if (localStorage.getItem("sesion") != "true") {
+        location.href = "../../../../presentacion/IniciarSesion.html";
+} else if (sessionStorage.getItem("sesion")) {
+    if (sessionStorage.getItem("sesion") != "true") {
+        location.href = "../../../../presentacion/IniciarSesion.html";
+    }
+} else {
+    location.href = "../../../../presentacion/IniciarSesion.html";
+}
