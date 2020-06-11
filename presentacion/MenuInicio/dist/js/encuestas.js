d3be36708b58ffc3ce9d30b1e1a56ccdc15088b8
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-06-11 17:33:03 -0500

    encuestas v3

--- presentacion/MenuInicio/dist/js/encuestas.js
+++ presentacion/MenuInicio/dist/js/encuestas.js
@@ -12,100 +12,178 @@ var btn_guardar = document.getElementById("btn-guardar");
 var barra_progreso = document.getElementById("barra-progreso");
 
 let formularios = [];
+datosUsuario = JSON.parse(localStorage.getItem('datos'));
+let bloqueActual;
+//para que al cargar la pagina muestre el formulario de datos generales primero
+readTextFile("Datos generales");
+let progreso = 0;
 function readTextFile(bloque) {
-    formularios = [];
-    var file = 'http://127.0.0.1:5500/presentacion/MenuInicio/src/encuesta.json';
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            var data = JSON.parse(rawFile.responseText);
-            var col = document.getElementById("accordion");
-            for (let index = 0; index < data["bloques"].length; index++) {
-                if (data["bloques"][index]["nombre-bloque"] == bloque) {
-                    col.innerHTML = '<div class="card">'
-                        + '<div class="card-header" id="headingOne">'
-                        + '<h5 class="mb-0">'
-                        + '<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">'
-                        + data["bloques"][index]["nombre-bloque"]
-                        + '</button>'
-                        + '</h5>'
-                        + '</div>'
-                        + '<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">'
-                        + '<div class="card-body" id = "preguntas-' + index + '">'
-                        + '</div>'
-                        + '</div>'
-                        + '</div>';
-                    break;
+        formularios = [];
+        bloqueActual = bloque;
+        var file = 'http://127.0.0.1:5500/presentacion/MenuInicio/src/encuesta.json';
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function () {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                var data = JSON.parse(rawFile.responseText);
+                var col = document.getElementById("accordion");
+                for (let index = 0; index < data["bloques"].length; index++) {
+                    if (data["bloques"][index]["nombre-bloque"] == bloque) {
+                        col.innerHTML = '<div class="card">'
+                            + '<div class="card-header" id="headingOne">'
+                            + '<h5 class="mb-0">'
+                            + '<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">'
+                            + data["bloques"][index]["nombre-bloque"]
+                            + '</button>'
+                            + '</h5>'
+                            + '</div>'
+                            + '<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">'
+                            + '<div class="card-body" id = "preguntas-' + index + '">'
+                            + '</div>'
+                            + '</div>'
+                            + '</div>';
+                        break;
+                    }
                 }
-            }
-            for (let index = 0; index < data["bloques"].length; index++) {
-                if (data["bloques"][index]["nombre-bloque"] == bloque) {
-                    var pregunta = document.getElementById("preguntas-" + index);
-                    for (let j = 0; j < data["bloques"][index]["preguntas"].length; j++) {
-                        formularios.push("form-" + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"]);
-                        pregunta.innerHTML += '<form id = "form-' + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"] + '"><label class="border-bottom pregunta">' + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"] + '</label><br></form>'
-                        for (let k = 0; k < data["bloques"][index]["preguntas"][j][j + 1][0]["respuestas"].length; k++) {
-                            let respuesta = data["bloques"][index]["preguntas"][j][j + 1][0]["respuestas"][k]["respuesta"];
-                            let valorRespuesa = data["bloques"][index]["preguntas"][j][j + 1][0]["respuestas"][k]["valor"];
-                            let form = document.getElementById("form-" + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"]);
-                            form.innerHTML += '<div class="form-check ">'
-                                + '<label class="form-check-label">'
-                                + '<input type="radio" class="form-check-input" value = "' + valorRespuesa + '" name = "' + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"] + '">'
-                                + respuesta
-                                + '</label>'
-                                + '</div>';
+                for (let index = 0; index < data["bloques"].length; index++) {
+                    if (data["bloques"][index]["nombre-bloque"] == bloque) {
+                        var pregunta = document.getElementById("preguntas-" + index);
+                        for (let j = 0; j < data["bloques"][index]["preguntas"].length; j++) {
+                            formularios.push("form-" + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"]);
+                            pregunta.innerHTML += '<form id = "form-' + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"] + '"><label class="border-bottom pregunta">' + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"] + '</label><br></form>'
+                            for (let k = 0; k < data["bloques"][index]["preguntas"][j][j + 1][0]["respuestas"].length; k++) {
+                                let respuesta = data["bloques"][index]["preguntas"][j][j + 1][0]["respuestas"][k]["respuesta"];
+                                let valorRespuesa = data["bloques"][index]["preguntas"][j][j + 1][0]["respuestas"][k]["valor"];
+                                let form = document.getElementById("form-" + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"]);
+                                form.innerHTML += '<div class="form-check ">'
+                                    + '<label class="form-check-label">'
+                                    + '<input type="radio" class="form-check-input" value = "' + valorRespuesa + '" name = "' + data["bloques"][index]["preguntas"][j][j + 1][0]["pregunta"] + '">'
+                                    + respuesta
+                                    + '</label>'
+                                    + '</div>';
+                            }
                         }
                     }
                 }
             }
         }
-    }
-    rawFile.send();
+        rawFile.send();
 }
-//para que al cargar la pagina muestre el formulario de datos generales primero
-readTextFile("Datos generales");
-let progreso = 0;
-
 function verificarRespuestas() {
     for (const key in formularios) {
         elementos = document.getElementById(formularios[key]).elements;
         let longitud = elementos.length;
         for (var i = 0; i < longitud; i++) {
             if (elementos[i].type == "radio" && elementos[i].checked == true) {
-                //VALORES DE LAS RESPUESTAS
-                alert(elementos[i].value);
+                //TODO: MANDAR DATOS AL PHP
+                enviarResultados(elementos[i].name, elementos[i].value, datosUsuario["idAreas"], bloqueActual);
+
             }
         }
     }
 }
 
 function validarDatos() {
+    let cont = 0;
+    let cantidadPreguntas;
     //TODO: VALIDAR SI TODOS LOS CHECKS ESTAN SELECCIONADOS}
     for (const key in formularios) {
+        cantidadPreguntas = formularios.length;
         elementos = document.getElementById(formularios[key]).elements;
         let longitud = elementos.length;
         for (var i = 0; i < longitud; i++) {
-            if (!elementos[i].type == "radio" && elementos[i].checked == true) {
-                return false;
+            if (elementos[i].type == "radio" && elementos[i].checked == true) {
+                cont++;
             }
-            return true;
         }
     }
+    if (cont == cantidadPreguntas) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
 //aumentar el progreso
 function progress(ev) {
-    if (validarDatos() == true) {
-        progreso += 10;
-        barra_progreso.style.width = progreso + "%";
-        barra_progreso.textContent = progreso + "%";
-        barra_progreso.setAttribute("aria-valuenow", progreso);
-        verificarRespuestas();
-        alert("OK")
-    }else{
+    if (validarDatos()) {
+        if (!verificarProgreso()) {
+            progreso += 10;
+            barra_progreso.style.width = progreso + "%";
+            barra_progreso.textContent = progreso + "%";
+            barra_progreso.setAttribute("aria-valuenow", progreso);
+            verificarRespuestas();
+            alert("OK")
+        } else {
+            alert("este bloque ya esta temriando");
+        }
+
+    } else {
         alert("Favor de contestar todas las preguntas del bloque")
     }
+}
+function verificarProgreso(bloque) {
+    Boolean: status;
+    alert(datosUsuario["id_usuarios"])
+    let params = "id_usuarios=" + datosUsuario["id_usuarios"] + "&bloque=" + bloque;
+    peticion = new XMLHttpRequest();
+    peticion.open(
+        "POST",
+        "http://mante.hosting.acm.org/SIIAA_backend/negocios/php/shared/encuesta/registroProgreso.php"
+    );
+    peticion.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+    );
+    peticion.onload = function () {
+        var response;
+        response = JSON.parse(peticion.responseText);
+
+        if (response.error) {
+            alert(response.error);
+            return;
+        }
+        status = response.status;
+        alert(status);
+        peticion.send(params);
+        return status;
+    };
+    peticion.onreadystatechange = function () {
+        if (peticion.readyState == 4 && peticion.status != 200) {
+            alert("Ocurrio un error, por favor inténtelo más tarde");
+        }
+    };
+    peticion.send(params);
+}
+function enviarResultados(pregunta, valor, idArea, bloque) {
+    alert(bloque);
+    let params = "bloque=" + bloque + "&id_areas=" + idArea + "&valor_respuesta=" + valor + "&pregunta=" + pregunta;
+    peticion = new XMLHttpRequest();
+    peticion.open(
+        "POST",
+        "http://mante.hosting.acm.org/SIIAA_backend/negocios/php/shared/encuesta/registroEncuestas.php"
+    );
+    peticion.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+    );
+    peticion.onload = function () {
+        var response;
+        response = JSON.parse(peticion.responseText);
+
+        if (response.error) {
+            alert(response.error);
+            return;
+        }
+
+    };
+    peticion.onreadystatechange = function () {
+        if (peticion.readyState == 4 && peticion.status != 200) {
+            alert("Ocurrio un error, por favor inténtelo más tarde");
+        }
+    };
+    peticion.send(params);
 }
 
 btn_b1.addEventListener("click", function () { readTextFile("Datos generales") });
